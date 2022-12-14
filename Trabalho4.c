dc5626f9feed0000266eba0e06612498e3fb1b0e
j0rge253 <113564823+j0rge253@users.noreply.github.com>
2022-12-13 21:37:25 -0300

    Add files via upload

---
+++ Trabalho4.c
@@ -0,0 +1,47 @@
+#include "ordvetor.h"
+
+VETORORD* VETORD_create(int n, COMP* compara) {
+	VETORORD* vetor = malloc(sizeof(VETORORD));
+	vetor->N = n;
+	vetor->P = 0;
+	vetor->elems = malloc(n * sizeof(void*));
+	vetor->comparador = compara;
+	return vetor;
+}
+
+void VETORD_add(VETORORD* vetor, void* newelem) {
+	if (vetor->P < vetor->N) {
+		int posicao = vetor->P;
+         
+		int i = 0;
+		while (i < vetor->P) {
+			if (vetor->comparador(vetor->elems[i], newelem) == -1) {
+				posicao = i;
+				break;
+			}
+			i++;
+		}
+
+        
+	
+		for (int i = vetor->P; i > posicao; i--) {
+			vetor->elems[i] = vetor->elems[i - 1];
+		}
+
+		vetor->elems[posicao] = newelem;
+		vetor->P++;
+	}	
+}
+
+
+void* VETORD_remove(VETORORD* vetor) {
+	void* menor = vetor->elems[0];
+
+	for (int i = 0; i < vetor->P - 1; i++) {
+		vetor->elems[i] = vetor->elems[i + 1];
+	}
+	
+	vetor->P--;
+
+	return menor;
+}
